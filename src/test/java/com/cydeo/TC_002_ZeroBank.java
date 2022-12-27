ce7c7db620537b71b9690672a84b5614b4bb7fe0
Mevlut Doger <mdoger@hotmail.com>
2022-12-27 02:10:41 +0000

    Completed TC_002_ZeroBank

---
+++ src/test/java/com/cydeo/TC_002_ZeroBank.java
@@ -0,0 +1,25 @@
+package com.cydeo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC_002_ZeroBank {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://zero.webappsecurity.com/login.html");
+        WebElement headerText = driver.findElement(By.tagName("h3"));
+        String expectedHeaderText = "Log in to ZeroBank";
+        String actualHeaderText = headerText.getText();
+        System.out.println(actualHeaderText);
+        if (actualHeaderText.equals(expectedHeaderText)) {
+            System.out.println("Header Text Verification PASSED!");
+        } else {
+            System.out.println("Header Text Verification FAILED!!!");
+        }
+    }
+}
